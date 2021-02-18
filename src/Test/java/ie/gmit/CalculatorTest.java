66e926d6c048308b80d8915f9a1f0b4c0387646a
Andy <g00345869@gmit.ie>
2021-02-18 14:44:13 +0000

    added testCode and updated POM to include plugin

--- src/Test/java/ie/gmit/CalculatorTest.java
+++ src/Test/java/ie/gmit/CalculatorTest.java
@@ -1,4 +1,43 @@
 package ie.gmit;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+
+    @Test
+    void testAdd(){
+        Calculator Mycal = new Calculator();
+        int result = Mycal.add(20,20);
+        assertEquals(40,result,0);
+
+    }
+
+    @Test
+    @Disabled
+    void testSubtract(){
+        Calculator MyCal = new Calculator();
+        int result = MyCal.Subtract(50,10);
+        assertEquals(40,result,0);
+    }
+
+    @Test
+    @Disabled
+    void testDivide(){
+        Calculator MyCal = new Calculator();
+        int result = MyCal.Divide(50,100);
+        assertEquals(2,result,0);
+    }
+
+    @Test
+    @Disabled
+    void testMultiply(){
+        Calculator MyCal = new Calculator();
+        int result = MyCal.Multiply(5,10);
+        assertEquals(50,result,0);
+    }
+
+
 }
--- src/main/java/ie/gmit/Calculator.java
+++ src/main/java/ie/gmit/Calculator.java
@@ -1,28 +1,25 @@
-/*
-Andrew Hughes
-11/02/2021
-g00345869@gmit.ie
-* */
-
 package ie.gmit;
 
 public class Calculator {
-    private int firstNum;
-    private int secondNum;
 
-    public Calculator(){
-        this.firstNum = 0;
-        this.secondNum = 0;
-    }
-
-    public Calculator(int firstNum, int secondNum) {
-        this.firstNum = firstNum;
-        this.secondNum = secondNum;
-    }
-
-    public int add(int num1, int num2){
-
-        int result = num1 + num2;
+    int add(int firstNum,int secondNum){
+        int result = firstNum + secondNum;
         return result;
     }
+
+    int Subtract(int firstNum, int secondNum){
+        int result = firstNum - secondNum;
+        return result;
+    }
+
+    int Divide(int firstNum, int secondNum){
+        int result = firstNum / secondNum;
+        return result;
+    }
+
+    int Multiply(int firstNum, int secondNum){
+        int result = firstNum * secondNum;
+        return result;
+    }
+
 }
